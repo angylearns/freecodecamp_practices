395d814fc42fc28bcff62cfec2e9a60181b40705
aams91 <99359053+angylearns@users.noreply.github.com>
2023-08-30 17:00:43 +0200

    The "Counting Cards" practice of the "JavaScript Algorithms and Data Structures" certificate.

--- JSADS/counting_cards.js
+++ JSADS/counting_cards.js
@@ -1,8 +1,13 @@
-/* 
-The "Counting Cards" practice from the Basic JavaScript section of the freeCodeCamp's JavaScript Algorithms and Data Structures certificate:
+// The "Counting Cards" practice from the Basic JavaScript section of the freeCodeCamp's JavaScript 
+// Algorithms and Data Structures certificate.
 
-You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
-*/
+// Instructions: 
+// "You will write a card counting function. It will receive a card parameter, which can be a number
+// or a string, and increment or decrement the global count variable according to a card's value.
+// The function will then return a string with the current count and the string Bet if the count
+// is positive, or Hold if the count is zero or negative. The current count and the player's decision
+// (Bet or Hold) should be separated by a single space."
+
 
 let count = 0;
 
